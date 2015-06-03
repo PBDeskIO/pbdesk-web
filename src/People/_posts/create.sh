edd51ccf3acd96759538fc2f4dcd1325a046bbcf
Pinal Bhatt <Pinal.Bhatt@weightwatchers.com>
2015-06-02 22:43:55 -0400

    people data

---
+++ src/People/_posts/create.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+# cp ../people_template.html  2015-06-02-xxx.html
+
+cd Z
+cd ..
+
+cd Y
+cd ..
+
+cd X
+cd ..
+
+cd W
+cd ..
+
+cd V
+cd ..
+
+cd U
+cd ..
+
+cd T
+cd ..
+
+cd S
+cd ..
+
+cd R
+cd ..
+
+cd Q
+cd ..
+
+cd P
+cd ..
+
+cd O
+cd ..
+
+cd M
+cd ..
+
+cd M
+cd ..
+
+cd L
+cd ..
+
+cd K
+cd ..
+
+cd J
+cd ..
+
+cd I
+cd ..
+
+cd H
+cd ..
+
+cd G
+cd ..
+
+cd F
+cd ..
+
+#cd E
+#cp ../people_template.html  2015-06-02-Edward-Charbeneau.html
+#cp ../people_template.html  2015-06-02-Eric-Ligman.html
+#cp ../people_template.html  2015-06-02-Emanuele-DelBono.html
+#cd ..
+#cd D
+#cp ../people_template.html  2015-06-02-Damian-Edwards.html
+#cp ../people_template.html  2015-06-02-Daren-May.html
+#cp ../people_template.html  2015-06-02-Dan-Fernandez.html
+#cd ..
+
+#cd C
+#cp ../people_template.html  2015-06-02-Clint-Rutkas.html
+#cp ../people_template.html  2015-06-02-Cody-Lindley.html
+#cp ../people_template.html  2015-06-02-Cheryl-Hammond.html
+#cp ../people_template.html  2015-06-02-Chris-Rose.html
+#cd ..
+
+#cd B
+#cp ../people_template.html  2015-06-02-Bart-De-Smet.html
+#cp ../people_template.html  2015-06-02-Bill-Wagner.html
+#cp ../people_template.html  2015-06-02-Brian-Keller.html
+#cp ../people_template.html  2015-06-02-Bob-Tabor.html
+#cp ../people_template.html  2015-06-02-Buddy-James.html
+#cd ..
+
+#cd A
+#cp ../people_template.html  2015-06-02-Andrea-Boschin.html
+#cp ../people_template.html  2015-06-02-Adam-Freeman.html
+#cp ../people_template.html  2015-06-02-Andrew-Church.html
+#cp ../people_template.html  2015-06-02-Andrew-Clear.html
+#cp ../people_template.html  2015-06-02-Agus-Kurniawan.html
+#cp ../people_template.html  2015-06-02-Alessandro-Del-Sole.html
+#cd ..
